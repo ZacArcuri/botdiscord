44ee4c58e62ec8cef62724758c0c583d96341312
Zac Arcuri <48154940+ZacArcuri@users.noreply.github.com>
2019-03-02 14:28:13 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.login(process.env.TOKEN)
+
+bot.on("message", function (message) {
+    message.channel.send( '@everyone WALLLL-E')
+        }
+  ) ;
+
